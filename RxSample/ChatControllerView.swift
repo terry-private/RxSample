87986de0d2987ece665e3a14cd1868b0b608b809
teruhito <ataeteruhito@gmail.com>
2021-08-12 10:37:06 +0900

    Remove "self" which does not need to be explicitly specified.

--- RxSample/ChatControllerView.swift
+++ RxSample/ChatControllerView.swift
@@ -32,15 +32,15 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITextViewDeleg
     }
     
     func createUI() {
-        self.navigationController?.isNavigationBarHidden = true
-        self.view.backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 0.92, alpha: 1.0)
+        navigationController?.isNavigationBarHidden = true
+        view.backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 0.92, alpha: 1.0)
         
         //**チャット UIView
-        let frameUIView = CGRect(x: 0, y : 0, width: self.view.frame.width, height: self.view.frame.height)
-        self.chatUIView = UIView(frame: frameUIView)
+        let frameUIView = CGRect(x: 0, y : 0, width: view.frame.width, height: view.frame.height)
+        chatUIView = UIView(frame: frameUIView)
         
         //TableView
-        let frame = CGRect(x: 0, y : 0, width: self.view.frame.width, height: self.view.frame.height - 50)
+        let frame = CGRect(x: 0, y : 0, width: view.frame.width, height: view.frame.height - 50)
         messageTableView = UITableView(frame: frame)
         messageTableView.backgroundColor = UIColor.clear
         messageTableView.allowsSelection = false;
@@ -50,13 +50,13 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITextViewDeleg
         chatUIView.addSubview(messageTableView!)
         
         //**入力 UIView
-        let frameUIViewInput = CGRect(x: 0, y : chatUIView.frame.height - 50, width: self.view.frame.width, height: 50)
+        let frameUIViewInput = CGRect(x: 0, y : chatUIView.frame.height - 50, width: view.frame.width, height: 50)
         inputUIView = UIView(frame: frameUIViewInput)
         inputUIView.backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 0.92, alpha: 1.0)
         chatUIView.addSubview(inputUIView)
         
         //テキスト
-        inputText = UITextView (frame: CGRect(x: 5, y: 5 , width: self.view.frame.width - 70, height: 40))
+        inputText = UITextView (frame: CGRect(x: 5, y: 5 , width: view.frame.width - 70, height: 40))
         inputText.font = UIFont.systemFont(ofSize: 18)
         inputText.autocorrectionType = UITextAutocorrectionType.no
         inputText.keyboardType = UIKeyboardType.default
@@ -64,12 +64,12 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITextViewDeleg
         inputUIView.addSubview(inputText)
         
         //送信ボタン
-        sendButton = UIButton(frame: CGRect(x: self.view.frame.width - 60,y: 5,width: 50,height:40))
+        sendButton = UIButton(frame: CGRect(x: view.frame.width - 60,y: 5,width: 50,height:40))
         sendButton.setTitle("送信", for: .normal)
         sendButton.setBackgroundColor(.systemRed, for: .normal)
         inputUIView.addSubview(sendButton)
         
-        self.view.addSubview(chatUIView)
+        view.addSubview(chatUIView)
         
     }
 }
